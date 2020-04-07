be601b3730b3558a11f2e358a66927a474f623ca
Richard Knoll <riknoll@users.noreply.github.com>
2020-04-07 16:53:20 -0700

    Adding text sprite

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
- 
+//% color=#3d8a59 block="Text Sprites"
+namespace simpleText {
+    //% blockId="simple_text_create_text"
+    //% block="create text sprite $text with foreground $foreground background $background and kind $kind"
+    //% kind.shadow=spritekind
+    //% foreground.shadow=colorindexpicker
+    //% foreground.defl=1
+    //% background.defl=0
+    //% background.shadow=colorindexpicker
+    //% text.defl="hello"
+    export function createTextSprite(text: string, foreground: number, background: number, kind: number): Sprite {
+        const font = image.getFontForText(text);
+        const textImage = image.create(font.charWidth * text.length, font.charHeight);
+        if (background) textImage.fill(background);
+        textImage.print("text", 0, 0, foreground, font);
+
+        return sprites.create(textImage, kind);
+    }
+} 
